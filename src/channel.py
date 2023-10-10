742d6377f3c3592da1094301298e12e2d2dbb2a2
Zhenya Kudrya <megusta997@gmail.com>
2023-10-10 18:42:12 +0300

    homework 2 change channel.py

--- src/channel.py
+++ src/channel.py
@@ -12,14 +12,25 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала.
         Дальше все данные будут подтягиваться по API."""
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.id_channel = self.channel["items"][0]['id']
-        self.title = self.channel["items"][0]["snippet"]['title']
-        self.channel_description = self.channel["items"][0]["snippet"]["description"]
-        self.url = self.channel["items"][0]["snippet"]["thumbnails"]["default"]["url"]
-        self.subscribers_count = self.channel["items"][0]["statistics"]["subscriberCount"]
-        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
-        self.quantity_views = self.channel["items"][0]["statistics"]["viewCount"]
+        self.__channel_id = channel_id
+        self._init_from_api
+
+    @property
+    def _init_from_api(self):
+
+        channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+
+        self.title = channel['items'][0]['snippet']['title']
+
+        self.description = channel['items'][0]['snippet']['description']
+
+        self.url = f'https://www.youtube.com/channel/{channel["items"][0]["id"]}'
+
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -27,7 +38,7 @@ class Channel:
 
     @property
     def channel_id(self):
-        return self.channel_id == channel_id
+        return self.__channel_id
 
     @classmethod
     def get_service(cls):
